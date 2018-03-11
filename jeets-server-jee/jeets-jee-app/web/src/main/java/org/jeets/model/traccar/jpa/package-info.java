3f1743d056a1816ddb5e0d3f1e6417ad4c906d34
Kristof Beiglböck <kbeigl@jeets.org>
2018-03-11 15:15:19 +0100

    removed fake PU from web

--- jeets-server-jee/jeets-jee-app/web/src/main/java/org/jeets/model/traccar/jpa/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This simulates the Persistence Unit and is not installed on the Application Server!
- * If you want to make use of the PU you can supply it in the lib folder of the ear file
- * and mark it as provided in the -ejb and -web projects. You can also add the WildFly
- * Datasource in the ear file or create a spi script (see activemq.rar) to install them.
- * 
- * @author kbeigl@jeets.org
- */
-package org.jeets.model.traccar.jpa;
